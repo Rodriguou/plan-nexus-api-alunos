a2a6ccde7149d9677248e3dd839614d915f907cf
gitmvfs <marcossantos8002@gmail.com>
2024-03-31 11:03:04 -0300

    feat: adicionado rota login ao servidor

--- src/server.js
+++ src/server.js
@@ -5,9 +5,13 @@ const dotnev = require("dotenv").config({ path: resolve(__dirname + "../") })
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
+//Import das rotas
+const alunoRota = require("./routes/alunoRoute")
+
+
 // variaveis de ambiente
 
-const port = process.env.PORT
+const port = process.env.PORT || 3334
 
 //Config de middlewares
 
@@ -15,5 +19,8 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Rotas
+
+app.use("/aluno",alunoRota)
 
 app.listen(port, () => console.log("Server listen on : " + port))
