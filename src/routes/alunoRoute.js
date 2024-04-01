6d973701d8cbaadb17d209d446c01a483a8a8610
gitmvfs <marcossantos8002@gmail.com>
2024-04-01 10:01:07 -0300

    feat(definirSenha): feito rota de definir senha

--- src/routes/alunoRoute.js
+++ src/routes/alunoRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { loginAluno } = require("../controller/controllerAluno")
+const { loginAluno, definirSenha } = require("../controller/controllerAluno")
 const novoErro = require("../utils/erros")
 const { alunoLoginValidacao } = require("../utils/validacao")
 
@@ -25,6 +25,34 @@ router.post("", async (req, res) => {
         const msg = err.issues ?? err.message 
         res.status(status).json(msg)
     }
+})
+
+router.patch("/definirSenha", async(req,res) =>{
+
+    try{
+        const {email, senha,token} = req.body
+        
+        const aluno = {
+            email,
+            senha,
+            token
+        }
+
+        const response = await definirSenha(aluno)
+
+        response[0] == 1
+        ? res.status(200).json({ "msg": "Atualizado com sucesso", "statusCode": 200 })
+        : res.status(400).json({ "msg": "Erro ao atualizar senha, verifique os campos.", "statusCode": 400 })
+
+
+    }
+    catch(err){
+        const status = err.status?? 500 
+        const msg = err.issues ?? err.message 
+        res.status(status).json(msg)
+    }
+
+
 })
 
 module.exports = router
